df4644e0941e42cd4d6ad734b8d9097861d98b0a
Shaun Poweshiek <Shaun.Poweshiek@ou.edu>
2020-09-03 17:42:12 -0500

    Saving finale changes

---
+++ ReadingFileExample/ReadingFileExample/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+
+namespace ReadingFileExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\Owner\Downloads\Pride and Prejudice by Jane Austen.txt";
+
+            //string entireBook = File.ReadAllText(filePath);
+            string[] allLines = File.ReadAllLines (filePath);
+
+            //for (int i = 0; i < allLines.Length; i++)
+            //{
+            //    if (i % 5 == 0 && i !=0)
+            //    {
+            //        Console.ReadKey();
+            //    }
+            //    Console.WriteLine(allLines[i]);
+            foreach (var line in allLines)
+            {
+                if(line.ToLower().Contains("chapter") == true)
+                {
+                    Console.ReadLine();
+                }
+                Console.WriteLine(line);
+            }
+            
+
+
+        }
+    }
+}
