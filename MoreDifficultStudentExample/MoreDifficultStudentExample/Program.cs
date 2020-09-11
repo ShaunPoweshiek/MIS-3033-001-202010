cac8e5c44a13b80514b9a64cee36c8ababfbbc29
Shaun Poweshiek <Shaun.Poweshiek@ou.edu>
2020-09-11 13:15:24 -0500

    1:15pm commit

---
+++ MoreDifficultStudentExample/MoreDifficultStudentExample/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MoreDifficultStudentExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student myStudent = new Student(5,"Adam","Ackerman",100000);
+            Console.WriteLine(myStudent);
+        }
+    }
+}
