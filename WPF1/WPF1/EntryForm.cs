b0c58447b3db2741ceb59daed1740891ad721c7d
Shaun Poweshiek <Shaun.Poweshiek@ou.edu>
2020-09-18 18:49:38 -0500

    Participation Commit

---
+++ WPF1/WPF1/EntryForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF1
+{
+    class EntryForm
+    { //Set class variables
+        public string name { get; set; }
+        public string address { get; set; }
+        public int zipCode { get; set; }
+
+        //default contructor.
+        public EntryForm()
+        {
+            name = string.Empty;
+            address = string.Empty;
+            zipCode = 0;
+        }
+
+        public EntryForm(string name, string address, int zipCode)
+        {
+            this.name = name;
+            this.address = address;
+            this.zipCode = zipCode;
+        }
+    }
+
+
+}
---
+++ WPF1/WPF1/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnter_Click(object sender, RoutedEventArgs e)
+        {
+            //Retrieve information the user enters into the text boxes.
+
+            string name = txtName.Text;
+            string address = txtAddress.Text;
+            int zipCode = txtZipCode.PersistId;
+
+            //Call the EntryForm class, using the following object:
+
+            EntryForm entry = new EntryForm(name, address, zipCode);
+
+            lstUserInfo.Items.Add(entry.name + " " + entry.address + " " + entry.zipCode);
+        }
+    }
+}
