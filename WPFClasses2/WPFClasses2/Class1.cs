eafabca993e3e7200a8fc242ebeaefb4665714f3
Shaun Poweshiek <Shaun.Poweshiek@ou.edu>
2020-09-25 19:54:33 -0500

    Commit

---
+++ WPFClasses2/WPFClasses2/Class1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WPFClasses2
+{//declaring and getting/setting constructors for the class
+    class Toy
+    {public string Manufacturer { get; set; }
+    public string Name { get; set; }
+    public double Price { get; set; }
+    private string Aisle { get; set; }
+//default constructor...
+    public Toy()
+        {
+            Manufacturer = "";
+            Name = "";
+            Price = 0;
+            Aisle = "";
+        }
+        //overload contructor...
+     public Toy(string manufacturer, string name, double price, string aisle)
+        {
+            manufacturer = Manufacturer;
+            name = Name;
+            price = Price;
+            aisle = Aisle;
+        }
+        //Override constructor...display info from user into the list box...
+        public override string ToString()
+        {
+            string sentence = $"Manufacturer: {Manufacturer}, Name: {Name}, Price: {Price}";
+            return sentence;
+        }
+
+        // Get AISLE info and display to a message box....I could not get this section of code to work correctly.  :(
+        //public string Aisle1()
+        //{
+        //    aisle = Manufacturer.Substring(0, 1).ToUpper();
+
+        //    aisle = Price.ToString();
+
+        //    aisle = Aisle;
+        //    return Aisle;
+        //}
+
+        //MessageBox.Show(SelectedToy.Aisle1());
+    }
+}
---
+++ WPFClasses2/WPFClasses2/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFClasses2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //Button click method
+        private void btnClickMe_Click(object sender, RoutedEventArgs e)
+        {//Calling the TOY class to take user entry and put it into the list box...
+            Toy toy1 = new Toy();
+            toy1.Manufacturer = txtManufacturer.Text;
+            toy1.Name = txtToyName.Text;
+            toy1.Price = Convert.ToDouble(txtToyPrice.Text);
+
+            lstDataList.Items.Add(toy1);
+
+        }
+    }
+}
