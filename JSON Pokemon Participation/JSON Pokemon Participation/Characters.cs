8a7e9266fe647b84efd8ad91d83a7840105b6380
Shaun Poweshiek <Shaun.Poweshiek@ou.edu>
2020-10-02 23:49:19 -0500

    Deadline Commit

---
+++ JSON Pokemon Participation/JSON Pokemon Participation/Characters.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSON_Pokemon
+{
+    class Characters
+    {
+        public Info info { get; set; }
+        public List<Character> results { get; set; }
+    }
+    public class Character
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public double weight { get; set; }
+        public double height { get; set; }
+        public string front_default { get; set; }
+
+        public override string ToString()
+        {
+            return name;
+        }
+    }
+
+    public class Info
+    {
+        public int count { get; set; }
+        public int pages { get; set; }
+        public string next { get; set; }
+        public string prev { get; set; }
+    }
+
+}
---
+++ JSON Pokemon Participation/JSON Pokemon Participation/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using JSON_Pokemon;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JSON_Pokemon
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string webService = @"https://pokeapi.co/api/v2/pokemon";
+
+            //HttpClient client = new HttpClient();
+            Characters characters;
+            using (var client = new HttpClient())
+            {
+                var results = client.GetStringAsync(webService).Result;
+
+                characters = JsonConvert.DeserializeObject<Characters>(results);
+            }
+
+            foreach (var character in characters.results)
+            {
+                cboCharacters.Items.Add(character);
+            }
+
+        }
+        private void cboCharacters_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var character = (Character)cboCharacters.SelectedItem;
+            var uri = new Uri(character.front_default);
+            var img = new BitmapImage(uri);
+            imgCharacter.Source = img;
+            lblHeightWeight.Content = $"{ character.name} has a height of {character.height}, and a weight of {character.weight}";
+        }
+    }
+}
+
