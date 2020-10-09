09c53820a037a9871e9c12263d70342115a21974
Shaun Poweshiek <Shaun.Poweshiek@ou.edu>
2020-10-09 18:34:36 -0500

    Participation Commit

---
+++ ChuckNorrisJokes/ChuckNorrisJokes/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static ChuckNorrisJokes.jokes;
+
+namespace ChuckNorrisJokes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private object JsonConvert;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string datasetUrl = "https://api.chucknorris.io/";
+
+            AllJokes alljokies
+;
+
+            using (var client = new HttpClient())
+            {
+                string json = client.GetStringAsync(datasetUrl).Result;
+
+                alljokies = JsonConvert.DeserializeObject<AllJokes>(json);
+            }
+
+            foreach (var item in alljokies.results)
+            {
+                cboJokeCategories.Items.Add(item);
+            }
+        }
+
+        private void cboJokeCategories_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selectedCategory = (ResultObject)cboJokeCategories.SelectedItem;
+
+            string datasetUrl2 = "https://api.chucknorris.io/jokes/categories" + selectedCategory;
+
+            RandomJoke randomJoke;
+
+            using (var client = new HttpClient())
+            {
+                string json = client.GetStringAsync(datasetUrl2).Result;
+
+                randomJoke = JsonConvert.DeserializeObject<RandomJoke>(json);
+            }
+        }
+
+        private void btnSelectMe_Click(object sender, RoutedEventArgs e)
+        {
+            string jokeApiUrl = @"https://api.chucknorris.io/jokes/random";
+
+            using (var client = new HttpClient())
+            {
+                string json = client.GetStringAsync(jokeApiUrl).Result;
+
+                var jokeQuote = JsonConvert.DeserializeObject<jokes>(json);
+                tboJokeOutput.Text = $"{value}";
+            }
+
+        }
+    }
+}
---
+++ ChuckNorrisJokes/ChuckNorrisJokes/jokes.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChuckNorrisJokes
+{
+    class jokes
+    {
+        public class AllJokes
+        {
+            public List<ResultObject> results { get; set; }
+        }
+
+        public class ResultObject
+        {
+            public string catagories { get; set; }
+
+            public string url { get; set; }
+
+            public override string ToString()
+            {
+                return catagories;
+            }
+        }
+
+        public class RandomJoke
+        {
+            public string value { get; set; }
+
+            public override string ToString()
+            {
+                string joke = $"{value}";
+                return joke;
+            }
+        }
+
+
+
+    }
+}
